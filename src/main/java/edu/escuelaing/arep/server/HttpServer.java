d65bbd6a4686c5646979971f680121e2e49bb74c
Cristhian Torres <cristhian.torres@mail.escuelaing.edu.co>
2022-02-23 16:46:19 -0500

    github y heroku

---
+++ src/main/java/edu/escuelaing/arep/server/HttpServer.java
@@ -0,0 +1,86 @@
+package edu.escuelaing.arep.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+    private int port;
+
+    private static HttpServer _instance = new HttpServer();
+
+    private HttpServer(){
+
+    }
+
+    private static HttpServer getInstance(){
+        return _instance;
+    }
+
+    public static void main(String... args) throws IOException{
+        HttpServer.getInstance().startServer(35000);
+    }
+
+    public void startServer(int httpPort) throws IOException {
+        port = httpPort;
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(getPort());
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        boolean running = true;
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir en puerto "+getPort()+" ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            outputLine = "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "\r\n"
+                    + "<!DOCTYPE html>\n"
+                    + "<html>\n"
+                    + "<head>\n"
+                    + "<meta charset=\"UTF-8\">\n"
+                    + "<title>Title of the document</title>\n"
+                    + "</head>\n"
+                    + "<body>\n"
+                    + "<h1>Mi propio mensaje</h1>\n"
+                    + "</body>\n"
+                    + "</html>\n";
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+}
